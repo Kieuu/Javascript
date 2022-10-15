8fc8a077368003b8322054f024534efbb775e29a
Kieuu <16030194@sv.ussh.edu.vn>
2022-10-15 15:02:36 +0700

    Create scripts3.js

---
+++ Vuive/scripts3.js
@@ -0,0 +1,10 @@
+// 1. Tạo 2 function tên và nội dung tùy ý theo 2 cách Function declaration và Function expression
+//Function declaration
+function Anh(x, y) { 
+    console.log (x-y)
+}
+
+// Function expression
+const Anh = function(x, y) {
+    console.log (x-y)	
+}
